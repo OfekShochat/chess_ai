8ad591e316309baf263a27a4cc126cc2724b234e
Ofek Shochat <50481015+OfekShochat@users.noreply.github.com>
2020-07-16 08:10:30 +0300

    Add files via upload

--- chess ai/main.py
+++ chess ai/main.py
@@ -1,5 +1,1 @@
-import chessengine
-from layers import layer
-from net import net
-network = net(1)
-network.bind(layer({"ws": {"kaki"}, "b":10}, "input"), layer({"ws": {"poop"}, "b":2}, "output"))
+import csnn
